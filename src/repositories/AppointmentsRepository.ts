127a241daee1ae5922734b20d11be952ba3689e7
guilhermebcabral <guibcabral19@gmail.com>
2020-11-25 18:24:35 -0300

    initial commit

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,34 @@
+import { isEqual } from 'date-fns'
+import Appointment from '../models/Appointment'
+
+// Repositório responsável pelo CRUD completo dos Appointments
+
+class AppointmentsRepository {
+  private appointments: Appointment[]
+
+  constructor() {
+    this.appointments = []
+  }
+
+  // Retornar todos agendamentos
+  public All(): Appointment[] {
+    return this.appointments
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(date, appointment.date)
+    )
+    return findAppointment || null
+  }
+
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date)
+
+    this.appointments.push(appointment)
+
+    return appointment
+  }
+}
+
+export default AppointmentsRepository
